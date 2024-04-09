b6be2d56bbdd0e01acee57f9f1f5ae58652938ec
bodysen <liang.ys@qq.com>
2024-04-09 21:47:48 +0800

    Update global-context.tsx

--- app/components/global-context.tsx
+++ app/components/global-context.tsx
@@ -1,17 +1,33 @@
 import { createContext, useContext, useState } from "react";
 
-const GlobalContext = createContext();
+interface GlobalContextType {
+  avatar: string;
+  updateAvatar: (newAvatar: string) => void;
+  nickname: string;
+  updateNickname: (newNickname: string) => void;
+  // 在这里添加其他全局变量的类型和相应的更新函数
+}
 
-export const GlobalProvider = ({ children }) => {
-  const [avatar, setAvatar] = useState('');
-  const [nickname, setNickname] = useState('');
+const initialGlobalState: GlobalContextType = {
+  avatar: '',
+  updateAvatar: () => {},
+  nickname: '',
+  updateNickname: () => {},
+  // 在这里添加其他全局变量的初始值
+};
+
+const GlobalContext = createContext<GlobalContextType>(initialGlobalState);
+
+export const GlobalProvider: React.FC = ({ children }) => {
+  const [avatar, setAvatar] = useState(initialGlobalState.avatar);
+  const [nickname, setNickname] = useState(initialGlobalState.nickname);
   // 在这里添加其他全局变量的状态和更新函数
 
-  const updateAvatar = (newAvatar) => {
+  const updateAvatar = (newAvatar: string) => {
     setAvatar(newAvatar);
   };
 
-  const updateNickname = (newNickname) => {
+  const updateNickname = (newNickname: string) => {
     setNickname(newNickname);
   };
 
@@ -25,3 +41,4 @@ export const GlobalProvider = ({ children }) => {
 };
 
 export const useGlobal = () => useContext(GlobalContext);
+
