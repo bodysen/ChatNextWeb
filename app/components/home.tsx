26d8d92536c30e8318525c33cf841ed473aad5c4
bodysen <liang.ys@qq.com>
2024-04-09 23:09:56 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -153,8 +153,6 @@ function Screen() {
         <>
           <SideBar 
             className={isHome ? styles["sidebar-show"] : "" }
-            nickname={nickname}
-            avatar={avatar} 
           />
 
           <div className={styles["window-content"]} id={SlotID.AppBody}>
